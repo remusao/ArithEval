e08d1ce8ecaba0027425665f2063b52eabf80ad9
Remi Berson <remi.berson@gmail.com>
2012-12-15 00:50:51 +0100

    Cleaned code, fix warnings.

--- src/main.cc
+++ src/main.cc
@@ -11,46 +11,58 @@
 // Define the type in which we will store our numbers
 typedef size_t numberType;
 
-int
-main()
+int main()
 {
 	size_t baseSize, exprSize, index, size;
 	char buffer[10];
 
-	// Read base
-	size = 4;
+
+	///////////////////
+	//   Read base   //
+	///////////////////
+	size = 4; // baseSize is at most 4 octets (including the trailing \n
 	baseSize = readInt(buffer, size, index);
-	// printf("%zi\n", baseSize);
     char* base = (char*)alloca(baseSize + 1);
 	for (size_t i = index; i < size; ++i)
 		base[i - index] = buffer[i];
 
 	read(STDIN_FILENO, base + (4 - index), baseSize - (4 - index) + 1);
     base[baseSize] = '\0';
-	// printf("%s\n", base);
-	write(STDOUT_FILENO, base, baseSize);
-	printf("\n");
 
-	// Read expression
+
+	/////////////////////////
+	//   Read expression   //
+	/////////////////////////
 	size = 10;
 	exprSize = readInt(buffer, size, index);
-	// printf("%zi\n", exprSize);
     char* expression = (char*)alloca(exprSize + 1);
 	for (size_t i = index; i < size; ++i)
 		expression[i - index] = buffer[i];
 
     read(STDIN_FILENO, expression + (10 - index), exprSize - (4 - index) + 1);
     expression[exprSize] = '\0';
-	write(STDOUT_FILENO, expression, exprSize);
-	printf("\n");
+
+
+	////////////////////////////////////////
+	//   Create optimisation structures   //
+	////////////////////////////////////////
+
+    // Allocate a tab of numberType to store the numbers
+    numberType* compressedExpression = (numberType*)alloca(0); // TODO
 
     // Convert bases
-    //numberType newBaseConverter[256] = {0};
-    //numberType oldBaseConverter[256] = {0};
-    //baseConverter(base, baseSize, newBaseConverter, oldBaseConverter);
+    numberType newBaseConverter[256] = {0};
+    numberType oldBaseConverter[256] = {0};
+    baseConverter(base, baseSize, newBaseConverter, oldBaseConverter);
 
-    // Lexing / Parsing / Evaluate
-    // TODO
+	
+	/////////////////////////////
+	//   Evaluate expression   //
+	/////////////////////////////
+
+	// TODO
+
+
 
     return 0;
 }
--- src/utils.cc
+++ src/utils.cc
@@ -7,10 +7,10 @@ size_t readInt(char buff[], size_t& size, size_t& index)
 	size_t res = 0;
 	index = 0;
 
-	if ((size = read(STDIN_FILENO, buff, size)))
+	if ((size = (size_t)read(STDIN_FILENO, buff, size)))
 	{
 		for (; index < size && buff[index] != '\n'; ++index)
-			res = res * 10 + (buff[index] - '0');
+			res = res * 10 + (size_t)(buff[index] - '0');
 		++index;
 	}
 
